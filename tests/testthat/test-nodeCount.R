8d7cb8c9de2458a9cdab33d5e7a26d829432fe85
fionarhuang <fionarhuang@gmail.com>
2018-06-19 19:27:56 +0200

    change unit test for nodeCount - update the arguments for nodeCount

--- tests/testthat/test-nodeCount.R
+++ tests/testthat/test-nodeCount.R
@@ -9,13 +9,13 @@ test_that("nodeCount works correctly", {
   colnames(count) <- paste(c("C1_", "C2_"),
   c(1:5, 1:5), sep = "")
 
-  expect_error(nodeCount(tipTable = count, tree = NULL,
-                         stree = NULL, fun = sum))
-  expect_equal(nodeCount(tipTable = count, tree = tinyTree,
-                         stree = NULL, fun = sum)[18, 1], 9)
-  expect_equal(nodeCount(tipTable = count, tree = tinyTree,
-                         stree = NULL, fun = sum)[nrow(count)+1, 1], sum(count[,1]))
-  expect_equal(nodeCount(tipTable = count, tree = tinyTree,
-                         stree = NULL, fun = mean)[nrow(count)+1, 1], mean(count[,1]))
+  expect_error(nodeCount(data = count, tree = NULL,
+                          fun = sum))
+  expect_equal(nodeCount(data = count, tree = tinyTree,
+                          fun = sum)[18, 1], 9)
+  expect_equal(nodeCount(data = count, tree = tinyTree,
+                          fun = sum)[nrow(count)+1, 1], sum(count[,1]))
+  expect_equal(nodeCount(data = count, tree = tinyTree,
+                          fun = mean)[nrow(count)+1, 1], mean(count[,1]))
 
 })
