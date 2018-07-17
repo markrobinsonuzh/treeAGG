fce97bd02c9a887dc8e79c641a919fb71233abbe
fionarhuang <fionarhuang@gmail.com>
2018-07-17 16:52:02 +0200

    add import ape

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' A simulated phylogenetic tree with 10 tips and 9 internal nodes
 #'
 #' A random phylo object created using the function \code{\link[ape]{rtree}}
-#'
+#' @importFrom ape rtree
 #' @format A phylo object with 10 tips and 9 internal nodes:
 #' \describe{
 #'   \item{Tip labels}{t1, t2, ..., t10.}
@@ -15,7 +15,7 @@
 #' A simulated phylogenetic tree with 50 tips and 49 internal nodes
 #'
 #' A random phylo object created using the function \code{\link[ape]{rtree}}
-#'
+#' @importFrom ape rtree
 #' @format A phylo object with 50 tips and 49 internal nodes:
 #' \describe{
 #'   \item{Tip labels}{t1, t2, ..., t50.}
@@ -27,7 +27,7 @@
 #' A simulated phylogenetic tree with 800 tips and 799 internal nodes
 #'
 #' A random phylo object created using the function \code{\link[ape]{rtree}}
-#'
+#' @importFrom ape rtree
 #' @format A phylo object with 800 tips and 799 internal nodes:
 #' \describe{
 #'   \item{Tip labels}{t1, t2, ..., t800.}
