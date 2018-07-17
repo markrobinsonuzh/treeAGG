da7497497c875fc203e53b4e681a27ac257653c5
fionarhuang <fionarhuang@gmail.com>
2018-07-17 14:05:26 +0200

    - change P.lim to pLim (name consistency)

--- R/treeAGG.R
+++ R/treeAGG.R
@@ -9,14 +9,14 @@
 #'        2. a column of p value
 #'        3. a column of adjusted p value
 #' @param stree A list of phylo object. The subtrees of \strong{tree}.
-#' @param P.lim The threshold value (for the adjusted p values) to reject a null
+#' @param pLim The threshold value (for the adjusted p values) to reject a null
 #'   hypothesis. By default, NULL. If NULL, the algorithm only compares the
 #'   value provided by \strong{varAGG} and doesn't decide whether to reject a
 #'   null hypothesis.
 #' @param varSIG The name of column for testing significance
 #' @param varAGG The name of column used to do tree aggregation, eg. the name of
 #'   the p value or adjusted p value column
-#' 
+#'
 #' @return A character vector, containing the labels of tips and nodes which
 #' have selected by the minimum P-value algorithm
 #'
@@ -24,13 +24,13 @@
 #'
 
 treeAGG <- function(tree, data, stree = NULL,
-                    P.lim = NULL, varSIG = NULL,
+                    pLim = NULL, varSIG = NULL,
                     varAGG) {
-  
+
   if (!inherits(tree, "phylo")) {
     stop("object tree is not of class phylo.")
   }
-  
+
   # if stree is not provided, generate it using pruneTree
   if (is.null(stree)) {
     #cat("stree is not provided and will be generated automatically")
@@ -38,23 +38,23 @@ treeAGG <- function(tree, data, stree = NULL,
   } else {
     stree <- stree
   }
-  
+
   isPhy <- unlist(lapply(stree, FUN = function(x) {
     inherits(x, "phylo")
   }))
   if (!all(isPhy)) {
     stop("object stree is not a list of phylo objects.")
   }
-  
-  if (!is.null(P.lim)) {
+
+  if (!is.null(pLim)) {
     if (is.null(varSIG)) {
-      stop("varSIG should be specified if P.lim is not null")
+      stop("varSIG should be specified if pLim is not null")
     }
   }
-  
+
   if (!is.null(varSIG)) {
-    if (is.null(P.lim)) {
-      stop("Specify a value for P.lim if varSIG is not null")
+    if (is.null(pLim)) {
+      stop("Specify a value for pLim if varSIG is not null")
     }
   }
   # tips & nodes from the whole tree
@@ -62,23 +62,23 @@ treeAGG <- function(tree, data, stree = NULL,
   num.node <- tree$Nnode
   all.tip <- tree$tip.label
   num.tip <- length(all.tip)
-  
+
   ## ----------- tree aggregation----------- firstly, set all tips and nodes as TRUE
   keep.tip <- rep(TRUE, num.tip)
   keep.node <- rep(TRUE, num.node)
   names(keep.tip) <- all.tip
   names(keep.node) <- all.node
   keep <- c(keep.tip, keep.node)
-  
+
   # compare between the parent and its children.  If the parent has smaller value,
   # set their children as FALSE otherwise set the parent as FALSE.
-  
-  for (i in 1:length(all.node)) {
+
+  for (i in seq_along(all.node)) {
     # parent & children
     node.i <- all.node[i]  # parent node
     tree.i <- stree[[node.i]]
     child.i <- setdiff(c(tree.i$node.label, tree.i$tip.label), node.i)
-    
+
     # add 1 here to avoid the NA p-value in tips / nodes (NA might be due to the
     # filteration in DESeq or not observed)
     rank <- data[, varAGG]
@@ -95,16 +95,21 @@ treeAGG <- function(tree, data, stree = NULL,
       }
     }
   }
-  
+
   keep1 <- keep[keep]
   namK <- names(keep1)
-  
-  if (is.null(P.lim)) {
+
+  if (is.null(pLim)) {
     final <- namK
   } else {
-    isSig <- rownames(data)[data[, varSIG] <= P.lim]
+    isSig <- rownames(data)[data[, varSIG] <= pLim]
     final <- intersect(isSig, namK)
   }
-  
-  return(final)
+
+   return(final)
+  # datF <- cbind.data.frame(label = rownames(data),
+  #                         select = ifelse(rownames(data) %in% final,
+  #                                         TRUE, FALSE))
+  #return(datF)
+
 }
