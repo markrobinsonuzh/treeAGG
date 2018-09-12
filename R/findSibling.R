761e3f2bdf242f5f890953fcbf10bb0b1b996cc4
fionarhuang <fionarhuang@gmail.com>
2018-09-12 16:52:40 +0200

    add findSibling function to find the sibling branch of a specified branch

---
+++ R/findSibling.R
@@ -0,0 +1,36 @@
+#' find the sibling node
+#'
+#' \code{findSibling} is to find the sibling node of an \code{input} node.
+#'
+#' @param tree A phylo object.
+#' @param input A numeric or character vector. Node labels or node numbers.
+#' @param label A logical value, TRUE or FALSE. If TRUE, node labels are
+#' returned.
+#'
+#' @keywords internal
+#' @return A vector of node labels or node numbers
+#'
+findSibling <- function(tree, input, label = FALSE){
+    # find descendant leaves of input
+    inT <- lapply(input,
+                  FUN = function(x){
+                      findOS(tree = tree,
+                             ancestor = x,
+                             only.Tip = TRUE)})
+    # find the parent node of the input
+    pN <- findAncestor(tree = tree, node = input, level = 1)
+
+    # Leaves not included in input
+    exT <- lapply(seq_along(pN), FUN = function(x) {
+        aT <- findOS(tree = tree, ancestor = pN[x], only.Tip = TRUE)
+        setdiff(aT, inT[[x]])
+    })
+
+    # replace leaves with their ancestor branch node
+    fT <- lapply(exT, FUN = function(x) {
+        signalNode(tree = tree, node = x, label = FALSE)} )
+    fT <- unlist(fT)
+    # isT <- isTip(tree = tree, input = fT)
+    # fn <- fT[!isT]
+    return(fT)
+}
