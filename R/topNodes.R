c25f34b96377075de3daa91a15a1085136cd4bab
fionarhuang <fionarhuang@gmail.com>
2018-09-12 16:51:04 +0200

    update topNodes to extract result tables from the rowData

--- R/topNodes.R
+++ R/topNodes.R
@@ -11,7 +11,7 @@
 #'   decreasing or not.
 #' @param col.rowData The names of columns to be extracted from \code{rowData}.
 #' @param col.linkData The names of columns to be extracted from \code{linkData}.
-#' @param show.which A numeric vector. It specifies which table to show. If
+#' @param use.assays A numeric vector. It specifies which table to show. If
 #'   NULL, the table correspoinding to the first number storing in the
 #'   \code{use.assays} (in \code{metadata} of \code{input} data) will be shown.
 #'   To recall, numbers in the \code{use.assays} record which matrix-like elements
@@ -23,35 +23,55 @@
 #'
 topNodes <- function(data, sort.by = "FDR", decreasing = FALSE,
                      col.rowData = NULL, col.linkData = NULL,
-                     show.which = NULL){
+                     use.assays = NULL){
 
-    use.assays <- metadata(data)$use.assays
-    if (is.null(show.which)) {
-        show.which <- use.assays[[1]]
+    if (is.null(use.assays)) {
+        use.assays <- 1
     }
-    aData <- assays(data)
-    cData <- lapply(seq_along(aData), FUN = function(x) {
-        if (x %in% show.which) {
 
-            xx <- cbind(aData[[x]], rowData(data)[ , col.rowData],
-                        linkData(data)[, col.linkData])
-            colnames(xx) <- c(colnames(aData[[x]]), col.rowData, col.linkData)
-            xx
-        } else { NULL }
+    # extract row data
+    rData <- rowData(data, internal = TRUE)
 
-    })
+    # the name of column that stores results we are interested
+    cNam <- paste("result_assay", use.assays, sep = "")
+    fData <- rData[, cNam, drop = FALSE]
 
-    kData <- lapply(seq_along(cData), FUN = function(x) {
-        #y <- x[x$aggKeep, ]
-        if (x %in% show.which) {
-            y <- cData[[x]]
-            oo <- order(y[, sort.by], decreasing = FALSE)
-            sy <- y[oo, ]
-            return(sy)
-        } else { NULL }
+    # convert to a list: results from different assays as different elements
+    list1 <- lapply(seq_len(ncol(fData)), FUN = function(x) {
+        fData[, x]
 
     })
-    return(kData)
+    names(list1) <- colnames(fData)
+
+    # convert to a list: results from different contrasts as different
+    # sub-elements
+    list2 <- lapply(list1, FUN = function(x) {
+        xx <- lapply(seq_len(ncol(x)), FUN = function(y) { x[, y]})
+        names(xx) <- colnames(x)
+        return(xx)
+    })
+
+    # add columns from rowData and linkData
+    list3 <- rapply(list2, function(x) {
+        xx <- cbind(x, rowData(data)[, col.rowData],
+                    linkData(data)[, col.linkData])
+        colnames(xx) <- c(colnames(x), col.rowData, col.linkData)
+        return(xx)
+    }, how = "list")
+
+    # sort according to a column specified by sort.by
+    list4 <- rapply(list3, function(x) {
+        if (! sort.by %in% colnames(x)) {
+            cat("Available columns: ", colnames(x), ".\n" )
+            stop("\n ", sort.by,
+                 " can't be found. Choose another from the available columns. \n" )
+        }
+         oo <- order(x[[sort.by]], decreasing = decreasing, na.last = TRUE)
+        fx <- x[oo, ]
+         return(fx)
+    }, how = "list")
+
+    return(list4)
 
 
 }
